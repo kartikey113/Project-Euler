59ce5deb0a6d08632f2eca068bd28912e1e44ab9
Hritesh kumar  Pramanik <pramanik.hritesh@gmail.com>
2019-10-01 00:13:07 +0530

    changed error

--- ques2.java
+++ ques2.java
@@ -3,12 +3,13 @@ class q2
 	public static void main(String[] args) 
 	{
 		int ft=1,st=2,nt,n=4000000,sum=0;
-		while(ft<=n)
+		while(ft<n)
 		{
 			if(ft%2==0)    
 				sum+=ft;
 			nt=ft+st;
-			ft=st=nt;
+			ft=st;
+			st=nt;
       break;
 		}
 		System.out.println("Result :"+sum);
