84260f1108fa72b2f51883aad16444a8ea7f3cc3
Hritesh kumar  Pramanik <pramanik.hritesh@gmail.com>
2019-10-01 00:11:03 +0530

    updated variable name

--- ques1.java
+++ ques1.java
@@ -2,8 +2,8 @@ class q1
 {
 	public static void main(String[] args) 
 	{
-		int belowvalue=1000,sum=0,i;
-		for(i=1;i<belowvalue;i++)
+		int n=1000,sum=0,i;
+		for(i=1;i<n;i++)
 		{
 			if(i%3==0 ||i%5==0)
 				sum+=i;
