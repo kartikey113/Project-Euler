1339939b3fc32e87bd782f64d28591c1e4d8fc11
kartikey113 <54543631+kartikey113@users.noreply.github.com>
2019-09-30 22:11:15 +0530

    Create ques1.java

---
+++ ques1.java
@@ -0,0 +1,13 @@
+class q1 
+{
+	public static void main(String[] args) 
+	{
+		int n=1000,sum=0,i;
+		for(i=1;i<n;i++)
+		{
+			if(i%3==0 ||i%5==0)
+				sum+=i;
+		}
+		System.out.println("Result :"+sum);
+	}
+}
