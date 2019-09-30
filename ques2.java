8ff15131e52cb3282e167d73f70f0fde0632d2c0
kartikey113 <54543631+kartikey113@users.noreply.github.com>
2019-09-30 22:13:18 +0530

    Create ques2.java

---
+++ ques2.java
@@ -0,0 +1,16 @@
+class q2 
+{
+	public static void main(String[] args) 
+	{
+		int ft=1,st=2,nt,n=4000000,sum=0;
+		while(ft<=n)
+		{
+			if(ft%2==0)    
+				sum+=ft;
+			nt=ft+st;
+			ft=st=nt;
+      break;
+		}
+		System.out.println("Result :"+sum);
+	}
+}
